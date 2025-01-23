45be2dc13f3b4c363e534df211db893cfafadfa5
Csilla Toth <csillaout@gmail.com>
2025-01-23 11:41:41 +0000

    path and query parameters

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# fastapi_udemy
-full stack fastAPI project
+# FastAPI Udemy
+
+practicing fastAPI
+
+Get method:
+
+- path parameters - value that we pass on the path and we can use it in our function like:
+
+  @app.get('/blog/{id}')
+  def index(id: int): - this is a type validation
+  return {'message':f"Blog with id {id}"}
+
+- predefined values: with Enum
+
+  class BlogType(str, Enum):
+  short = 'short'
+  story = 'story'
+  howto = 'howto'
+
+  @app.get('/blog/type/{type}')
+  def blog_type(type: BlogType):
+  return {'message': f"Blog type: {type}"}
+
+- query parameters: everything you write fater the / separated by ? and &
+  (any function not part of the path are query parameters)
+  it can be page_size, page
+  @app.get('/blog/all')
+  def get_blogs(page, page_size):
+  return {'message': f"All {page_size} blogs on page {page}"}
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI #import the classe
+from typing import Optional
+from fastapi import FastAPI #import the class
+from enum import Enum
 
 app = FastAPI() #create an object/instance from the fastAPI
 
-@app.get('/') #endpoint, this provides our path
+#predefine paramaters:
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/hello') #endpoint, this provides our path
 def index():  #function
-    return "Hello World!"
+    return {'message':"Hello World!"}
+
+#@app.get('/blog/all')
+#def get_all_blogs():
+#    return {'message':'All blogs provided'}
+@app.get('/blog/all')
+def get_blogs(page=1, page_size: Optional[int] = None):
+    return {'message': f"All {page_size} blogs on page {page}"}
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return {'message': f"blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}"}
+
+@app.get('/blog/type/{type}')
+def blog_type(type: BlogType):
+    return {'message': f"Blog type: {type}"}
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'message':f"blog with id {id}!"}
+
